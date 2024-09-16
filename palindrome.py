0dd35234cd4b0660d07189cc183d0215f8bbc061
sahash07 <raikapilkhutru9863329002@gmail.com>
2024-09-16 08:59:58 +0545

    update

--- palindrome.py
+++ palindrome.py
@@ -15,6 +15,11 @@ while i<=10:
     print(f"new total={total}, i={i}")
 print(i)
 
+
+
+
+# Factorial of 4
+
 def factorial(n):
    
     if n == 0:
@@ -24,3 +29,25 @@ def factorial(n):
 
 result = factorial(4)
 print("The factorial of 4 is:", result)
+
+# Even number generating
+
+def generate_even_numbers(n):
+   
+    even_numbers = [i for i in range(0, n+1, 2)]
+    return even_numbers
+
+
+even_numbers = generate_even_numbers(100)
+print(even_numbers)
+
+i = 0
+while i<=100:
+    i = i+1
+    if i==50:
+        break
+    elif i%2==0:
+        continue
+        
+    else:
+        print(i)
